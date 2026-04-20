8ecc2fdddd03e920f4c7a53f52345f789f21d79b
Dudacostar <dudadacostar@gmail.com>
2026-04-20 17:08:35 -0300

    Update Cadastro.tsx

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -12,7 +12,7 @@ const Cadastro = () => {
   const handleSalvar = async (e: FormEvent) => {
   e.preventDefault();
 
-  const response = await fetch("https://backend-projetos.onrender.com/register", {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
